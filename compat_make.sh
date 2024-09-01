3514a730d73c3ffae31e1473f826ff5469263e98
Anubhab Bandyopadhyay <4890833+AnubhabB@users.noreply.github.com>
2024-09-01 21:13:20 +0530

    Cleanup

--- compat_make.sh
+++ compat_make.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # Prepares data for compatibility tests
-echo "Making data with Rust"
-cd rust
-cargo run make --release
-cd ..
-
 echo "Making data with Go"
 cd go
 go run main.go make
@@ -18,3 +13,8 @@ cd ..
 
 echo "Making data with Python"
 py/venv/bin/python3 py/compat.py make
+
+echo "Making data with Rust"
+cd rust
+cargo run make --release
+cd ..
